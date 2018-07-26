edecf384791c3951e8c1f0694783f95fe004a1d8
pufang <pufang@maihaoche.com>
2018-07-26 18:45:13 +0800

    跟进rocketmq 4.3.0 版本

--- src/main/java/com/maihaoche/starter/mq/config/MQProducerAutoConfiguration.java
+++ src/main/java/com/maihaoche/starter/mq/config/MQProducerAutoConfiguration.java
@@ -1,16 +1,22 @@
 package com.maihaoche.starter.mq.config;
 
 import com.maihaoche.starter.mq.annotation.MQProducer;
+import com.maihaoche.starter.mq.annotation.MQTransactionProducer;
+import com.maihaoche.starter.mq.base.AbstractMQTransactionProducer;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * Created by yipin on 2017/6/29.
@@ -43,4 +49,35 @@ public class MQProducerAutoConfiguration extends MQBaseAutoConfiguration {
         return producer;
     }
 
+    @PostConstruct
+    public void configTransactionProducer() {
+        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(MQTransactionProducer.class);
+        if(CollectionUtils.isEmpty(beans)){
+            return;
+        }
+        ExecutorService executorService = new ThreadPoolExecutor(beans.size(), beans.size()*2, 100, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2000), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setName("client-transaction-msg-check-thread");
+                return thread;
+            }
+        });
+        Environment environment = applicationContext.getEnvironment();
+        beans.entrySet().forEach( transactionProducer -> {
+            try {
+                AbstractMQTransactionProducer beanObj = AbstractMQTransactionProducer.class.cast(transactionProducer.getValue());
+                MQTransactionProducer anno = beanObj.getClass().getAnnotation(MQTransactionProducer.class);
+
+                TransactionMQProducer producer = new TransactionMQProducer(environment.resolvePlaceholders(anno.producerGroup()));
+                producer.setNamesrvAddr(mqProperties.getNameServerAddress());
+                producer.setExecutorService(executorService);
+                producer.setTransactionListener(beanObj);
+                producer.start();
+                beanObj.setProducer(producer);
+            } catch (Exception e) {
+                log.error("build transaction producer error : {}", e);
+            }
+        });
+    }
 }
